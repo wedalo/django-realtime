fbc0ae1ac98a4b98407038f7190955081f07bd65
Juan Diego H <jdiegoh3@users.noreply.github.com>
2018-06-08 12:04:06 -0500

    Changed return in a tag, fixed format

--- drealtime/templatetags/drealtimetags.py
+++ drealtime/templatetags/drealtimetags.py
@@ -30,7 +30,7 @@ def ishout_js():
     SSL, be sure to set `ISHOUT_HTTPS` to True.
     """
     path = _determine_base_path()
-    return """
+    return str("""
     <script type="text/javascript" src="%s/socket.io/socket.io.js"></script>
     <script type="text/javascript" src="%s/client/ishout.client.js"></script>
-    """ % (path, path)
+    """ % (path, path)).encode("utf-8")
